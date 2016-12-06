e44c8bd9dcf9a5c003d78561106463901ef7e75f
Andre <dre.board@gmail.com>
2016-12-05 20:08:08 -0500

    removal of development files

--- wp/wp-config-sample.php
+++ wp/wp-config-sample.php
@@ -29,7 +29,8 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', '1234');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+//define('DB_HOST', 'localhost');
+define('DB_HOST', '127.0.0.1');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
