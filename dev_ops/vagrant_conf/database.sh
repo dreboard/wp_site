4f7665d1e0f193d29e252825f564d85868b17727
Andre <dre.board@gmail.com>
2017-07-21 00:57:22 -0400

    removed log files

--- dev_ops/vagrant_conf/database.sh
+++ dev_ops/vagrant_conf/database.sh
@@ -3,18 +3,19 @@ echo "================= START INSTALL-MYSQL.SH $(date +"%r") ================="
 echo "==========================================================================="
 echo " "
 echo " "
+echo " "
 
 DBNAME='wordpress'
 PASSWORD='1234'
 DBUSER='root'
 
+echo "---------------- Install mysql-server"
 # install mysql and give password to installer
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
 sudo apt-get -y install mysql-server
 
-# install phpmyadmin and give password(s) to installer
-# for simplicity I'm using the same password for mysql and phpmyadmin
+echo "---------------- Install phpmyadmin"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
@@ -22,9 +23,22 @@ sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
 sudo apt-get -y install phpmyadmin
 
-echo -e "\n--- Setting up our MySQL user and db ---\n"
-mysql -uroot -p$PASSWORD -e "CREATE DATABASE $DBNAME" >> /vagrant/vm_build.log 2>&1
-mysql -uroot -p$PASSWORD -e "grant all privileges on $DBNAME.* to '$DBUSER'@'localhost' identified by '$PASSWORD'" > /vagrant/vm_build.log 2>&1
+
+echo "================= Create Database and install SQL Dumps ================="
+ RESULT=`mysql -u$DBUSER -p$PASSWORD -e "SHOW DATABASES" | grep -Fo $DBNAME`
+ if [ "$RESULT" == "$DBNAME" ]; then
+    echo "---------------- Database exist"
+ else
+    echo -e "\n--- Setting up our MySQL user and db ---\n"
+    mysql -uroot -p$PASSWORD -e "CREATE DATABASE $DBNAME" >> /vagrant/vm_build.log 2>&1
+    mysql -uroot -p$PASSWORD -e "grant all privileges on $DBNAME.* to '$DBUSER'@'localhost' identified by '$PASSWORD'" > /vagrant/vm_build.log 2>&1
+
+    #If database does not exist, perform dump of back up copy
+    echo "---------------- Installing dump"
+    mysql -u$DBUSER -p$PASSWORD $DBNAME < /vagrant/dev_ops/sql/wordpress.sql
+ fi
+
+sudo /etc/init.d/apache2 restart
 
 echo " "
 echo " "
